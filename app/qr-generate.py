7ee3d4451c10ffeabf4e602f3375773b420cb9b1
vadim <shooherboom@gmail.com>
2025-07-28 00:54:14 +0300

    update

--- app/qr-generate.py
+++ app/qr-generate.py
@@ -2,11 +2,15 @@ import json
 import base64
 import qrcode
 
-
+print("For generate QR-code input username and password")
+username = input("Input username:")
+password = input("Input password:")
 data = {
-    "username": "",
-    "password": base64.b64encode("".encode()).decode()
+    "username": username,
+    "password": base64.b64encode(password.encode()).decode()
 }
 
 qr = qrcode.make(json.dumps(data))
 qr.save("rdp_credentials.png")
+
+print("Generate QR-code Finish! File create!")
--- app/readme.md
+++ app/readme.md
@@ -1,2 +1,15 @@
-Depends:
+Create app/conf.ini file with the following structure:
+
+[RDP_Settings]
+RDP_SERVER = """ip adress or name server"""
+RDP_PORT = """server port"""
+DOMAIN = """DOMAIN name"""
+WIDTH = 
+HEIGHT = 
+
+Add to app/ logo file, logo.png and icon file icon.ico
+
+For generate qr-code use qr-generate script
+
+Depends for Ubuntu install:
 python3 python3-tk python3-pil python3-opencv freerdp2-x11 python3-pyzbar python3-pil.imagetk
